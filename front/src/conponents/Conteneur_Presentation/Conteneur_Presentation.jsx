2f117896fff862dc2f620673fe890dabacc651e1
delby3006-del <delby3006@gmail.com>
2026-04-03 14:27:32 +0200

    creation conteneur presentation

--- front/src/conponents/Conteneur_Presentation/Conteneur_Presentation.jsx
+++ front/src/conponents/Conteneur_Presentation/Conteneur_Presentation.jsx
@@ -1,13 +1,12 @@
 import "./Conteneur_Presentation.css";
+import Conteneur_Presentation_Images from "../Conteneur_Presentation_Images/Conteneur_Presentation_Images";
+import Conteneur_Presentation_Text from "../Conteneur_Presentation_text/Conteneur_Presentation_Text";
 
 export default function Conteneur_Presentation() {
   return (
     <div className="conteneur-presentation">
-      <img
-        src="/images/glace3B.png"
-        alt="Glace 3 boules de différentes saveurs"
-        className="image-presentation"
-      />
+      <Conteneur_Presentation_Images />
+      <Conteneur_Presentation_Text />
     </div>
   );
 }
---
+++ front/src/conponents/Conteneur_Presentation_Text/Conteneur_Presentation_Text.jsx
@@ -0,0 +1,15 @@
+import "./Conteneur_Presentation_Text.css";
+
+export default function Conteneur_Presentation_Text() {
+  return (
+    <div className="conteneur-presentation-text">
+      <h1 className="titre-presentation">
+        Bienvenue dans notre magasin de glaces !
+      </h1>
+      <p className="description-presentation">
+        Découvrez notre sélection de glaces artisanales, préparées avec des
+        ingrédients de qualité supérieure.
+      </p>
+    </div>
+  );
+}
